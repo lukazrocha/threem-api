b86c864f81df02daf43dbec1c9e399e07f69e47e
Lukaz Rocha <lukazrocha05@gmail.com>
2023-02-05 15:34:44 -0300

    :rocket: Initial Configuration

---
+++ src/main/kotlin/br/dev/lukazrocha/threem/ThreemApplication.kt
@@ -0,0 +1,11 @@
+package br.dev.lukazrocha.threem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ThreemApplication
+
+fun main(args: Array<String>) {
+	runApplication<ThreemApplication>(*args)
+}
---
+++ src/test/kotlin/br/dev/lukazrocha/threem/ThreemApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.dev.lukazrocha.threem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ThreemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
