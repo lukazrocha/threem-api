55597621cfbac04ec285d12625ab5392862c2970
Lukaz Rocha <lukazrocha05@gmail.com>
2023-02-11 12:19:42 -0300

    removing Category uneeded repository

--- src/main/kotlin/br/dev/lukazrocha/threem/repository/ExpenseCategoryRepository.kt
+++ src/main/kotlin/br/dev/lukazrocha/threem/repository/ExpenseCategoryRepository.kt
@@ -2,6 +2,8 @@ package br.dev.lukazrocha.threem.repository
 
 import br.dev.lukazrocha.threem.model.ExpenseCategory
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 import java.util.*
 
+@Repository
 interface ExpenseCategoryRepository : JpaRepository<ExpenseCategory, UUID>
